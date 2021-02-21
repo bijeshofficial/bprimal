31c1354d044aac7f7d275adff47afee3705f18ed
bijeshofficial <bijeshr.kunwar@gmail.com>
2021-02-21 18:06:03 +0545

    added docstring to maths package

--- maths/__init__.py
+++ maths/__init__.py
@@ -1,14 +1,26 @@
 def add(num1, num2):
+    """
+    Returns the sum of two numbers.
+    """
     return num1+num2
 
 
 def subtract(num1, num2):
+    """
+    Returns the difference of two numbers.
+    """
     return num1-num2
 
 
 def multiply(num1, num2):
+    """
+    Returns the multiplication of two numbers.
+    """
     return num1 * num2
 
 
 def divide(num1, num2):
+    """
+    Return the division of two numbers.
+    """
     return num1 / num2
